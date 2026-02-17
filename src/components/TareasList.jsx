acd9c0d100b7f3807c5a766ac8f721423ac58f0b
Lorena <230255@fppiramide.com>
2026-02-17 11:07:24 +0100

    Cositas

---
+++ src/components/TareasList.jsx
@@ -0,0 +1,10 @@
+const tareas = [
+    { id: 1, nombre: "Tarea 1", acabada: false },
+    { id: 2, nombre: "Tarea 2", acabada: true}
+]
+
+export default function TareasList() {
+    return(
+        <h1>Lista de tareas</h1>
+    )
+}
