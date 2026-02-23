0ed68843a87be2f9fc6f09be5499d7f15789ca25
Lorena <230255@fppiramide.com>
2026-02-23 09:28:01 +0100

    Algunas peticiones

--- src/servicios/peticiones.js
+++ src/servicios/peticiones.js
@@ -1,5 +1,24 @@
 import { URL_SERVER } from "../Constantes";
 
+export function login(email) {
+    return fetch(`${URL_SERVER}usuarios?email=${encodeURIComponent(email)}`)
+        .then(res => {
+            if (!res.ok) throw new Error("No se ha podido conectar con el servidor.");
+            return res.json();
+        })
+}
+
+export function registro(usuario) {
+    return fetch(`${URL_SERVER}usuarios`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(usuario)
+    }).then(res => {
+        if(!res.ok) throw new Error("No se ha podido registrar al usuario.");
+        return res.json();
+    })
+}
+
 export function obtenerTarea(idUser) {
     return fetch(`${URL_SERVER}tareas?id_user=${idUser}`)
         .then(res => res.json());
