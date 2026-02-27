4b6e7a365e419537a52722967faac9e0f2691e00
Lorena <230255@fppiramide.com>
2026-02-27 14:12:40 +0100

    Usuario de prueba creado, funciona correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,20 @@ import Navbar from './components/Navbar'
 import { Outlet } from 'react-router';
 function App() {
 
-  // opción 1
-  // const [isLogged, setIsLogged] = useState(false);
-
   // opcion 2
   // const [usuario, setUsuario] = useState(JSON.parse(sessionStorage.getItem("usuario") || {}));
-  const [usuario, setUsuario] = useState(
-    JSON.parse(sessionStorage.getItem("usuario") || "null")
-  );
+
+  // <button onClick={() => navigate("/")}>Ir a inicio</button>
+
+  // const [usuario, setUsuario] = useState(
+  //   JSON.parse(sessionStorage.getItem("usuario") || "null")
+  // );
+
+  // FIXME: prueba para el usuario
+  const [usuario, setUsuario] = useState({nombre: "patata"});
 
   return (
     <>
-      {/* <Login/> */}
-
-      {/* Hace falta un rederizado condicional, por si está logeado sale una cosa, si no otra */}
-
-      {/* Opción 1 */}
-      {/* (isLogged ? <DatosUsuario/> : <Login/>) */}
-
       
       {/* Opción 2 */}
       {/* Necesita el setter que va a guardar el usuario, si vamos al loggin, es que estamos setteando el usuario, ya que aun no hay uno logeado */}
@@ -34,8 +30,6 @@ function App() {
       {/* Opción 2 */}
       {/* Necesita el setter que va a guardar el usuario, si vamos al loggin, es que estamos setteando el usuario, ya que aun no hay uno logeado */}
       {/* {usuario.email ? <> <DatosUsuario usuario={usuario} setUsuario={setUsuario} /> <TareasList usuario={usuario} /> </> : <Login setUsuario={setUsuario} />} */}
-
-      {/* <TareasList usuario={usuario}/> */}
 
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,17 @@
 import { Link } from "react-router";
 
-// export default function Navbar({ usuario, setUsuario }) {
-//     return (
-
-//         <ul>
-//             {usuario ? (
-//                 <>
-//                     <li><Link to="/">Inicio</Link></li>
-//                     <li><Link to="/tareas">Tareas</Link></li>
-//                     <li><Link to="/logout" onClick={() => {sessionStorage.removeItem("usuario"); setUsuario(null)}}>Cerrar sesión</Link></li>
-//                 </>
-//             ) : (
-//                 <>
-//                     <li><Link to="/">Inicio</Link></li>
-//                     <li><Link to="/login">Login</Link></li>
-//                     <li><Link to="/registro">Registro</Link></li>
-//                 </>
-//             )}
-
-//         </ul>
-
-//     )
-// }
-
 export default function Navbar({ usuario, setUsuario }) {
     return (
 
         <ul>
-
-            <li><Link to="/">Inicio</Link></li>
-            {usuario ? (<li><Link to="/tareas">Tareas</Link></li>) : (<li><Link to="/login">Login</Link></li>)}
-            {usuario ? (<li><Link to="/logout" >Cerrar sesión</Link></li>) : (<li><Link to="/registro">Registro</Link></li>)}
-
+            <Link to="/">Inicio</Link>
+            {usuario ? (<Link to="/tareas">Tareas</Link>) : (<Link to="/login">Login</Link>)}
+            {usuario ? (
+                <Link to="/" onClick={() => {
+                    sessionStorage.removeItem("usuario"); setUsuario(null)
+                }} >Cerrar sesión</Link>
+            ) : (
+                <Link to="/registro">Registro</Link>)}
         </ul>
 
     )
