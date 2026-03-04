db795d1c08e1ad7341f123cd8e6016a03320d91a
Lorena <230255@fppiramide.com>
2026-03-04 10:06:06 +0100

    arreglado formulario

--- src/components/formularios/Registro/Registro.jsx
+++ src/components/formularios/Registro/Registro.jsx
@@ -14,39 +14,30 @@ export default function Registro({ setUsuario }) {
                     <label>Nombre</label>
                     <input type="text" value={usuarioNuevo.nombre} onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, usuarioNuevo: e.target.value })} />
                 </div>
-                
                 <div className="formulario">
                     <label>Apellidos</label>
                     <input type="text" value={usuarioNuevo.apellidos} onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, usuarioNuevo: e.target.value })} />
                 </div>
-         
                 <div className="formulario">
                     <label>Email</label>
                     <input type="text" value={usuarioNuevo.email} onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, usuarioNuevo: e.target.value })} />
                 </div>
-
                 <div className="formulario">
                     <label>Repetir email</label>
                     <input type="text" value={emailRepetir} onChange={(e) => setEmailRepetir(e.target.value)} />
                 </div>
-                
                 <div className="formulario">
                     <label>Contraseña</label>
                     <input type="password" value={usuarioNuevo.password} onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, usuarioNuevo: e.target.value })} />
                 </div>
-                
-                
                 <div className="formulario">
                     <label>Repetir contraseña</label>
                     <input type="password" value={passwordRepetir} onChange={(e) => setPasswordRepetir(e.target.value)} />
                 </div>
-                
-                
-                <div className="formulario">
+                <div className="condiciones">
                     <label>Condiciones</label>
                     <input type="checkbox" value={usuarioNuevo.condiciones} onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, usuarioNuevo: e.target.value })} />
                 </div>
-                
                 <div className="boton">
                     <button onClick={() => {
 
