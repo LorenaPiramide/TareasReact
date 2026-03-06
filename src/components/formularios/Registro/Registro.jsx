229eb9fe5e68842b0fdbbac5add8379af1edc3d1
Lorena <230255@fppiramide.com>
2026-03-06 13:50:00 +0100

    Validaciones hasta contraseña funcionan, falta dejarlo bonito y las demás

--- src/components/formularios/Registro/Registro.jsx
+++ src/components/formularios/Registro/Registro.jsx
@@ -25,15 +25,73 @@ export default function Registro() {
         const valor = e.target.value;
         let error = "";
 
-        if (!valor || valor.trim() === "") {
-            error = "El nombre es obligatorio";
+        if (!valor) {
+            error = "El nombre es obligatorio.";
         } else if (valor.length < 3) {
-            error = "El nombre debe tener al menos 3 caracteres";
+            error = "El nombre debe tener al menos 3 caracteres.";
         } else if (valor[0] !== valor[0].toUpperCase()) {
-            error = "El nombre debe empezar por mayúscula";
+            error = "El nombre debe empezar por mayúscula.";
         }
 
         setErrores({ ...errores, nombre: error });
+    }
+
+    // TODO: Creo que se puede hacer de otra forma
+    const validarApellidos = (e) => {
+        const valor = e.target.value;
+        let error = "";
+
+        if (!valor) {
+            error = "El apellido es obligatorio.";
+        } else if (valor.length < 3) {
+            error = "El apellido debe tener al menos 3 caracteres.";
+        } else if (valor[0] !== valor[0].toUpperCase()) {
+            error = "El apellido debe empezar por mayúscula.";
+        }
+
+        setErrores({ ...errores, apellidos: error });
+    }
+
+    // TODO: Comprobar si el email ya está registrado o no
+    const validarEmail = (e) => {
+        const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        const valor = e.target.value;
+        let error = "";
+
+        if (!valor) {
+            error = "El email es obligatorio.";
+        } else if (!regex.test(valor)) {
+            error = "El email no es válido.";
+        }
+
+        setErrores({ ...errores, email: error });
+    }
+
+    const validarRepetirEmail = (e) => {
+        const valor = e.target.value;
+        let error = "";
+
+        if (!valor) {
+            error = "Es obligatorio repetir el email.";
+        } else if (valor !== usuarioNuevo.email) {
+            error = "Los emails no coinciden.";
+        }
+
+        setErrores({ ...errores, emailRepetir: error })
+    }
+
+    const validarPassword = (e) => {
+        const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/;
+        const valor = e.target.value;
+        let error = "";
+
+        if (!valor) {
+            error = "La contraseña es obligatoria."
+        } else if (!regex.test(valor)) {
+            error = "La contraseña debe tener, al menos, 8 carácteres, incluyendo una letra mayúscula, una minúscula, un número y un carácter especial."
+        }
+
+        setErrores({ ...errores, password:  error })
     }
     
     const registrar = () => {
@@ -57,27 +115,63 @@ export default function Registro() {
                         value={usuarioNuevo.nombre} 
                         onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo,nombre:e.target.value})}
                         onBlur={validarNombre}
-                        className={errores.nombre ? "input_error" : ""} // Hay que hacer un input_error en css para poner el borde de input en rojo
+                        className={errores.nombre ? "inputError" : ""} // Hay que hacer un input_error en css para poner el borde de input en rojo
                     />
-                    <span id="nombreError" className="error_mensaje">
-                        {errores.nombre} {/* Aquí se muestra el mensaje de error */}
+                    {/* TODO: Me parece que no necesito el id del span */}
+                    <span className="errorMensaje">
+                        {errores.nombre} {/* Mensaje del return del error */}
                     </span>
                 </div>
                 <div className="formulario">
                     <label>Apellidos</label>
-                    <input type="text" value={usuarioNuevo.apellidos} onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, apellidos: e.target.value })} />
+                    <input 
+                        type="text" 
+                        value={usuarioNuevo.apellidos} 
+                        onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, apellidos: e.target.value })}
+                        onBlur={validarApellidos}
+                        className={errores.apellidos ? "inputError" : ""}
+                    />
+                    <span className="errorMensaje">
+                        {errores.apellidos}
+                    </span>
                 </div>
                 <div className="formulario">
                     <label>Email</label>
-                    <input type="text" value={usuarioNuevo.email} onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, email: e.target.value })} />
+                    <input 
+                        type="text" value={usuarioNuevo.email} 
+                        onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, email: e.target.value })}
+                        onBlur={validarEmail}
+                        className={errores.email ? "inputError" : ""}
+                    />
+                    <span className="errorMensaje">
+                        {errores.email}
+                    </span>
                 </div>
                 <div className="formulario">
                     <label>Repetir email</label>
-                    <input type="text" value={emailRepetir} onChange={(e) => setEmailRepetir(e.target.value)} />
+                    <input 
+                        type="text" 
+                        value={emailRepetir} 
+                        onChange={(e) => setEmailRepetir(e.target.value)}
+                        onBlur={validarRepetirEmail}
+                        className={errores.emailRepetir ? "inputError" : ""}
+                    />
+                    <span className="errorMensaje">
+                        {errores.emailRepetir}
+                    </span>
                 </div>
                 <div className="formulario">
                     <label>Contraseña</label>
-                    <input type="password" value={usuarioNuevo.password} onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, password: e.target.value })} />
+                    <input 
+                        type="password" 
+                        value={usuarioNuevo.password} 
+                        onChange={(e) => setUsuarioNuevo({ ...usuarioNuevo, password: e.target.value })}
+                        onBlur={validarPassword}
+                        className={errores.password ? "inputError" : ""}
+                    />
+                    <span className="errorMensaje">
+                        {errores.password}
+                    </span>
                 </div>
                 <div className="formulario">
                     <label>Repetir contraseña</label>
